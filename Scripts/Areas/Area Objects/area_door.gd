7e0373b5eb1259bc0db8008d9129b9058472bdeb
FlashBash21 <nathanolson55379@gmail.com>
2024-02-11 01:25:49 -0600

    Doors

--- Scripts/Areas/Area Objects/area_door.gd
+++ Scripts/Areas/Area Objects/area_door.gd
@@ -4,7 +4,9 @@ class_name AreaDoor
 @export var Area: int
 
 func _process(delta) -> void:
-	if (get_overlapping_bodies().size() > 0):
+	var bodies = get_overlapping_bodies()
+	if (bodies.size() > 0):
+		var body = bodies[0] as Entity
 		var parent = get_parent() as BaseArea
 		if !parent:
 			push_error("Door is not a child of a BaseArea node")
@@ -12,4 +14,6 @@ func _process(delta) -> void:
 		if !Area:
 			push_error("No Area defined for door")
 			return
+		if (body):
+			body.position = get_viewport().get_size() - Vector2i(body.position)
 		parent.query_area_load.emit(Area)
