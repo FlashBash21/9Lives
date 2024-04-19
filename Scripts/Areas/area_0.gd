492c0ff096734dd9139d7c93f0a990942550a6c9
olso9261 <olso9261@umn.edu>
2024-04-18 19:33:55 -0500

    Fixes

--- Scripts/Areas/area_0.gd
+++ Scripts/Areas/area_0.gd
@@ -2,10 +2,6 @@ extends BaseArea
 
 
 func _ready() -> void:
-	var enemy = load("res://Scenes/Entites/test_enemy.tscn")
-	var enemy_ref = enemy.instantiate() 
-	add_child(enemy_ref)
-	enemy_ref.position = Vector2(200, 200)
 	super()
 	var boss = load_entity("boss_enemy")
 	var enemy = load_entity("test_enemy")
